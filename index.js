58acde2fe16014d0f9b060cb93097bc047c92dbf
swanithalanka <swanitha@voxglobaltech.com>
2023-09-19 18:57:53 +0530

    packages

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express')
+const mongoose = require('mongoose')
+const Book = require("./models/books");
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
