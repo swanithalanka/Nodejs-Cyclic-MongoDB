27ac1a420a5dd65efcec8c8f784c0718dc29b248
swanithalanka <swanitha@voxglobaltech.com>
2023-09-20 12:03:02 +0530

    books

--- index.js
+++ index.js
@@ -17,3 +17,45 @@ const connectDB = async () => {
     process.exit(1);
   }
 }
+
+//Routes go here
+app.get('/', (req,res) => {
+    res.send({ title: 'Books' });
+})
+
+app.get('/books', async (req,res)=> {
+
+  const book = await Book.find();
+
+  if (book) {
+    res.json(book)
+  } else {
+    res.send("Something went wrong.");
+  }
+  
+});
+
+app.get('/add-note', async (req,res) => {
+  try {
+    await Book.insertMany([
+      {
+        title: "Sons Of Anarchy",
+        body: "Body text goes here...",
+      },
+      {
+        title: "Games of Thrones",
+        body: "Body text goes here...",
+      }
+    ]);
+    res.json({"Data":"Added"})
+  } catch (error) {
+    console.log("err", + error);
+  }
+})
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
